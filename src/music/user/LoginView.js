473f4e2ceb996ca6cd3f95d506684f452c5251cd
DESKTOP-LFV1CN5\USER <tnrlaos@gmail.com>
2023-03-28 01:17:28 +0900

    색깔 변경 및 회원가입, 아이디 비번 찾기 태그 추가

--- src/music/user/LoginView.js
+++ src/music/user/LoginView.js
@@ -2,17 +2,36 @@ import React from "react";
 import "../../css/loginView.css";
 
 const LoginView = () => {
+  const login = () => {
+    console.log("login");
+  };
+
   return (
     <div className="loginType">
-      <p className="logo">Music</p>
+      <a href="/" className="logo">
+        Music Reco
+      </a>
       <div>
         <input id="putId" type="text" placeholder="아이디를 입력해주세요"></input>
       </div>
       <div>
         <input id="putPass" type="password" placeholder="비밀번호는 8자리 이상"></input>
       </div>
+      <div className="joinOrFind">
+        <a href="/login" id="register">
+          회원가입
+        </a>
+        <a href="/login" id="findId">
+          아이디 찾기
+        </a>
+        <a href="/login" id="findPassword">
+          비밀번호 찾기
+        </a>
+      </div>
       <div>
-        <button className="loginButton">로그인</button>
+        <button className="loginButton" onClick={login}>
+          로그인
+        </button>
       </div>
     </div>
   );
