63a1f91c185a8e6c1b090c5b1f7e7d8ef3b78196
DESKTOP-LFV1CN5\USER <tnrlaos@gmail.com>
2023-03-27 23:42:23 +0900

    로그인 화면 진행중

--- src/music/user/LoginView.js
+++ src/music/user/LoginView.js
@@ -1,10 +1,19 @@
 import React from "react";
+import "../../css/loginView.css";
 
 const LoginView = () => {
   return (
-    <div>
-      <label>id</label>
-      <input></input>
+    <div className="loginType">
+      <p className="logo">Music</p>
+      <div>
+        <input id="putId" type="text" placeholder="아이디를 입력해주세요"></input>
+      </div>
+      <div>
+        <input id="putPass" type="password" placeholder="비밀번호는 8자리 이상"></input>
+      </div>
+      <div>
+        <button className="loginButton">로그인</button>
+      </div>
     </div>
   );
 };
