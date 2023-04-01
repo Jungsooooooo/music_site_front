06adbaeaf35cedcd6a2faa722ebfdb08cf589e28
DESKTOP-LFV1CN5\USER <tnrlaos@gmail.com>
2023-04-01 17:25:11 +0900

    유저 아이디와 비밀번호 생성 기능 구현 완료

--- src/music/user/MakeUser.js
+++ src/music/user/MakeUser.js
@@ -1,10 +1,22 @@
 import "../../css/MakeUser.css";
+import axios from "axios";
 
 const MakeUser = () => {
+  const createUser = () => {
+    const input = {
+      id: document.getElementById("makeId").value,
+      password: document.getElementById("makePass").value,
+    };
+
+    return axios.post("/api/user", input).then((res) => {
+      console.log(res);
+    });
+  };
+
   return (
     <>
       <div className="topColor"></div>
-      <div className="loginType">
+      <div className="joinType">
         <a href="/" className="logo">
           Music Reco
         </a>
@@ -19,36 +31,36 @@ const MakeUser = () => {
 
         <div>
           <h3>비밀번호 확인</h3>
-          <input id="checkPass" type="password" placeholder="비밀번호는 8자리 이상"></input>
+          <input id="checkPass" type="password"></input>
         </div>
 
         <div>
           <h3>이름</h3>
-          <input id="makeName" type="password" placeholder="비밀번호는 8자리 이상"></input>
+          <input id="makeName" type="text"></input>
         </div>
 
         <div>
           <h3>생년월일</h3>
-          <input id="birth" type="password" placeholder="비밀번호는 8자리 이상"></input>
+          <input id="birth" type="text"></input>
         </div>
 
         <div>
           <h3>성별</h3>
-          <input id="sex" type="password" placeholder="비밀번호는 8자리 이상"></input>
+          <input id="sex" type="text"></input>
         </div>
 
         <div>
           <h3>본인확인 이메일</h3>
-          <input id="email" type="password" placeholder="비밀번호는 8자리 이상"></input>
+          <input id="email" type="text"></input>
         </div>
 
         <div>
           <h3>휴대전화</h3>
-          <input id="phoneNumber" type="password" placeholder="비밀번호는 8자리 이상"></input>
+          <input id="phoneNumber" type="text"></input>
         </div>
 
         <div>
-          <button className="loginButton" onClick={""}>
+          <button className="loginButton" onClick={createUser}>
             가입하기
           </button>
         </div>
