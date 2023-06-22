3ba3d2f8999b8dcc31fc94a5a59d0369be0a1c27
DESKTOP-LFV1CN5\USER <tnrlaos@gmail.com>
2023-06-22 23:44:05 +0900

    관리자 - 유저 테이블 관리

--- src/music/admin/UserInfo.js
+++ src/music/admin/UserInfo.js
@@ -4,6 +4,7 @@ import TableBody from "@mui/material/TableBody";
 import TableCell from "@mui/material/TableCell";
 import TableHead from "@mui/material/TableHead";
 import TableRow from "@mui/material/TableRow";
+import { DataGrid } from "@mui/x-data-grid";
 
 import { useEffect } from "react";
 import axios from "axios";
@@ -22,23 +23,41 @@ const UserInfo = () => {
     return axios.get("/api/user");
   };
 
+  const columns = [
+    { field: "id", headerName: "ID", width: 90 },
+    {
+      field: "address",
+      headerName: "address",
+      width: 500,
+      editable: true,
+    },
+    {
+      field: "lastName",
+      headerName: "Last name",
+      width: 150,
+      editable: true,
+    },
+    {
+      field: "age",
+      headerName: "Age",
+      type: "number",
+      width: 110,
+      editable: true,
+    },
+    {
+      field: "fullName",
+      headerName: "Full name",
+      description: "This column has a value getter and is not sortable.",
+      sortable: false,
+      width: 160,
+      valueGetter: (params) => `${params.row.firstName || ""} ${params.row.lastName || ""}`,
+    },
+  ];
+
   return (
-    <Table id="userTable" style={{ margin: "auto", width: "800px" }}>
-      <TableHead>
-        <TableRow>
-          <TableCell>Uid</TableCell>
-          <TableCell>Id</TableCell>
-        </TableRow>
-      </TableHead>
-      <TableBody>
-        {rows.map((row) => (
-          <TableRow key={row.uid}>
-            <TableCell>{row.uid}</TableCell>
-            <TableCell>{row.id}</TableCell>
-          </TableRow>
-        ))}
-      </TableBody>
-    </Table>
+    <div id="userTable">
+      <DataGrid rows={rows} columns={columns} />
+    </div>
   );
 };
 
