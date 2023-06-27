ee73520ddc93e4eb7e6e204d12cef239bf782c5d
DESKTOP-LFV1CN5\USER <tnrlaos@gmail.com>
2023-06-27 23:37:58 +0900

    아이디 찾기 화면 구현 진행중

--- src/music/user/FindUser.js
+++ src/music/user/FindUser.js
@@ -1,22 +1,37 @@
 import React from "react";
+import { Button } from "@mui/material";
+import axios from "axios";
 
 import "../../css/FindUser.css";
 
 const FindUser = () => {
+  const findId = () => {
+    const input = {
+      name: document.getElementById("findIdName").value,
+      email: document.getElementById("findIdEmail").value,
+    };
+
+    return axios.post("/api/users/findid", input).then((res) => {
+      console.log(res.data);
+    });
+  };
+
   return (
-    <>
-      <div className="findUser">
-        <div>
-          이름
-          <input id="findIdName" type="text" placeholder="아이디를 입력해주세요"></input>
-        </div>
-        <div>
-          <h3>비밀번호</h3>
-          <input id="findIdEmail" type="password" placeholder="비밀번호는 8자리 이상"></input>
-        </div>
-        <div></div>
+    <div className="findUser">
+      <div>
+        <label className="findUserLableName">이름</label>
+        <input id="findIdName" type="text" placeholder="아이디를 입력해주세요"></input>
       </div>
-    </>
+      <div>
+        <label className="findUserLableName">이메일</label>
+        <input id="findIdEmail" type="text" placeholder="비밀번호는 8자리 이상"></input>
+      </div>
+      <div>
+        <Button variant="outlined" className="findButton" onClick={findId}>
+          찾기
+        </Button>
+      </div>
+    </div>
   );
 };
 
